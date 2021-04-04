265f0f1cc5694f26919eed4e0763911cbaf261bd
Tsatsu Kofi Amable <tsatsu.amable@gmail.com>
2021-04-04 01:24:19 +0100

    first commit

---
+++ src/main/java/com/locode/apps/schoolmaster/SchoolmasterApplication.java
@@ -0,0 +1,13 @@
+package com.locode.apps.schoolmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/locode/apps/schoolmaster/SchoolmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locode.apps.schoolmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
